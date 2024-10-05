fb4563c7aee678803dbc6a85048cd15d211e05a4
CormacSharkey <csharkey@tcd.ie>
2024-10-05 19:58:15 +0100

    adding echoes to bash script

--- assignment/java-project/exe.sh
+++ assignment/java-project/exe.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 echo "Purging for fresh run"
-
 rm -rf ../query-results/*
 rm -rf ../index/*
+echo "Finished purge"
 
 echo "Executing build and run"
-
 mvn clean package
-
 java -jar target/java-project-1.0.jar
-
 echo "Finished execution"
 
+echo "Calling trec_eval on query results"
 cd /home/csharkey/trec_eval/trec_eval-9.0.7/
-
 ./trec_eval -m runid -m gm_map -m P.5 /home/csharkey/InfoAssignments/CS7IS3-Info-Retrieval-Web-Search/assignment/cran/cranqrel_trec_eval /home/csharkey/InfoAssignments/CS7IS3-Info-Retrieval-Web-Search/assignment/query-results/Standard-BM25-res.txt
