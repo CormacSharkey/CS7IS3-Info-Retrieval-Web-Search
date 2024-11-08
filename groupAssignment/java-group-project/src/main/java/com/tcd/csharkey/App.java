776e44dadf62713fa3fdb197c592b8342ce0174f
CormacSharkey <csharkey@tcd.ie>
2024-11-08 14:23:08 +0000

    adding qrels file

--- groupAssignment/java-group-project/src/main/java/com/tcd/csharkey/App.java
+++ groupAssignment/java-group-project/src/main/java/com/tcd/csharkey/App.java
@@ -1,16 +1,5 @@
 package com.tcd.csharkey;
 
-import java.io.File;
-import java.util.ArrayList;
-
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.document.StringField;
-import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
-import org.jsoup.nodes.Element;
-
 public class App 
 {
     public static void main( String[] args ) {
