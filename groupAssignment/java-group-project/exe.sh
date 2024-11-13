20f1caefb26def5b8b561775102cf88840b5e123
CormacSharkey <csharkey@tcd.ie>
2024-11-13 18:01:56 +0000

    updating bash script

--- groupAssignment/java-group-project/exe.sh
+++ groupAssignment/java-group-project/exe.sh
@@ -9,3 +9,7 @@ echo "Executing build and run..."
 mvn clean package
 java -jar target/java-group-project-1.0.jar
 echo "Finished execution"
+
+echo "Calling trec_eval on query results..."
+cd ../trec_eval-9.0.7/
+./trec_eval -m runid -m map <insert qrel file path here> <insert query results file path here>
