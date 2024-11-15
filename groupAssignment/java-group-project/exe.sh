8a1d6e87d60ab47c04f7a4a7c32bd909c73990f1
CormacSharkey <csharkey@tcd.ie>
2024-11-15 21:43:49 +0000

    fixing bash

--- groupAssignment/java-group-project/exe.sh
+++ groupAssignment/java-group-project/exe.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# echo "Purging for fresh run..."
-# rm -rf ../index/*
-# rm -rf ../results/*
-# echo "Finished purge"
+echo "Purging for fresh run..."
+rm -rf ../index/*
+rm -rf ../results/*
+echo "Finished purge"
 
-# echo "Executing build and run..."
-# mvn clean package
-# java -jar target/java-group-project-1.0.jar -Xmx4g
-# echo "Finished execution"
+echo "Executing build and run..."
+mvn clean package
+java -jar target/java-group-project-1.0.jar -Xmx4g
+echo "Finished execution"
 
 echo "Calling trec_eval on query results..."
 cd ../trec_eval-9.0.7/
