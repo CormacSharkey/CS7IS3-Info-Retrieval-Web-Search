51ff21c8c23532ffd0f922e7691ef5a12bd5ea68
CormacSharkey <csharkey@tcd.ie>
2024-11-22 23:31:27 +0000

    attempting to fix files

--- groupAssignment/java-group-project/src/main/java/com/tcd/csharkey/App.java
+++ groupAssignment/java-group-project/src/main/java/com/tcd/csharkey/App.java
@@ -1,24 +1,55 @@
 package com.tcd.csharkey;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenFilterFactory;
+import org.apache.lucene.analysis.TokenizerFactory;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.BooleanSimilarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.search.similarities.LMDirichletSimilarity;
+import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
+import org.apache.lucene.search.similarities.MultiSimilarity;
+import org.apache.lucene.search.similarities.PerFieldSimilarityWrapper;
 import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.search.similarities.TFIDFSimilarity;
 
 public class App 
 {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException{
 
-        Analyzer analyzer = new EnglishAnalyzer();
-        Similarity score = new BM25Similarity();
+        // Specify the analyzer
+        Path resources = Paths.get("../data/");
+        Analyzer analyzer = CustomAnalyzer.builder(resources)
+            .withTokenizer("standard")
+            .addTokenFilter("lowercase")
+            .addTokenFilter("stop", "words", "stop_words.txt", "format", "wordset")
+            .addTokenFilter("porterstem")
+            .addTokenFilter("kStem")
+            .build();
 
+        // Specify the similarity scorer with a tweaked lambda value
+        Similarity[] scores = {new LMJelinekMercerSimilarity(0.63f)};
+        Similarity score = new MultiSimilarity(scores);
+
+        // Create a ParseIndexDocs object
         ParseIndexDocs parserIndexer = new ParseIndexDocs();
+
+        // Pass the document code and analzyer to the object to parse each document and add it to the index
         parserIndexer.CallParsers("fbis", analyzer);
         parserIndexer.CallParsers("fr", analyzer);
         parserIndexer.CallParsers("ft", analyzer);
         parserIndexer.CallParsers("lat", analyzer);
 
+        // Create a QueryTopics Object
         QueryTopics queryTopics = new QueryTopics();
+
+        // Pass the analyzer and similarity scorer to the object to parse the queries and query the index
         queryTopics.CallQueries(analyzer, score);
 
     }
--- groupAssignment/java-group-project/src/main/java/com/tcd/csharkey/ParseIndexDocs.java
+++ groupAssignment/java-group-project/src/main/java/com/tcd/csharkey/ParseIndexDocs.java
@@ -11,6 +11,7 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.BoostQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.jsoup.Jsoup;
@@ -19,27 +20,32 @@ import org.jsoup.nodes.Element;
 
 public class ParseIndexDocs {
 
+    // Specify the file paths to each document collection
     private String fbisPath = "../data/fbis";
     private String frPath = "../data/fr94";
     private String ftPath = "../data/ft";
     private String latPath = "../data/latimes";
+
+    // Specify the file path to the index
     String indexPath = "../index";
     
-    public void FBISParser(String filePath, Analyzer analyzer) {
+    // Method to parse the FBIS document collection - given the document collection file path, the analyzer and the index writer
+    public void FBISParser(String filePath, Analyzer analyzer, IndexWriter iwriter) {
+        // Get a list of all files in the root file collection directory
         File dir = new File(filePath);
         File[] fileList = dir.listFiles();
 
+        // Declare some variables for parsing the document collection
         org.jsoup.nodes.Document document;
         Elements elements;
-
         String id;
         String title;
         String author;
         String body;
-
         ArrayList<Document> documentsList = new ArrayList<Document>();
         Document doc = new Document();
 
+        // For loop - loop through each file in the directory, if it starts with "fb", parse the contents into a document list and add it to the index
         try {
             for (File file: fileList) {
                 if (file.getName().startsWith("fb")) {
@@ -63,24 +69,29 @@ public class ParseIndexDocs {
 
                         doc = new Document();
                     }
-                    writeToIndex(documentsList, analyzer);
+                    // writeToIndex(documentsList, analyzer);
+                    iwriter.addDocuments(documentsList);
                     documentsList = new ArrayList<>();
                 }
             }
         }
+        // Catch any exceptions
         catch (Exception e) {
-            System.out.println("Error: " + e);
+            System.out.println("Error in FBISParser: " + e);
         }
     }
 
-    public void FRParser(String filePath, Analyzer analyzer) {
+    // Method to parse the FR document collection - given the document collection file path, the analyzer and the index writer
+    public void FRParser(String filePath, Analyzer analyzer, IndexWriter iwriter) {
+        // Get a list of all folders in the root file collection directory
         File dir = new File(filePath);
         File[] dirList = dir.listFiles();
 
+        // Declare an array to store the file list for every subsequent directory in the root directory
         ArrayList<String> fileList = new ArrayList<>();
 
+        // If there's files in the root directory, loop through each file, check it's a directory and add the file contents to a list
         if (dirList != null) {
-
             for (int i=0; i<dirList.length; i++) {
                 if (dirList[i].isDirectory()) {
                     for (File f: dirList[i].listFiles()) {
@@ -90,18 +101,18 @@ public class ParseIndexDocs {
             }
         }
 
+        // Declare some variables for parsing the document collection
         File file;
         org.jsoup.nodes.Document document;
         Elements elements;
-        
         String id;
         String title;
         String author;
         String body;
-
         ArrayList<Document> documentsList = new ArrayList<Document>();
         Document doc = new Document();
 
+        // For loop - loop through each file in the list, parse the contents into a document list and add it to the index
         try {
             for (String name: fileList) {
                 file = new File(name);
@@ -119,29 +130,35 @@ public class ParseIndexDocs {
                     doc.add(new TextField("author", author, Field.Store.YES));
 
                     body = el.getElementsByTag("TEXT").text().replaceAll("[^a-zA-Z ]", "").toLowerCase();
+
                     doc.add(new TextField("body", body, Field.Store.YES));
 
                     documentsList.add(doc);
 
                     doc = new Document();
                 }
-                writeToIndex(documentsList, analyzer);
+                // writeToIndex(documentsList, analyzer);
+                iwriter.addDocuments(documentsList);
                 documentsList = new ArrayList<>();
             }
         }
+        // Catch any exceptions
         catch (Exception e) {
-            System.out.println("Error: " + e);
+            System.out.println("Error in FRParser: " + e);
         }
     }
 
-    public void FTParser(String filePath, Analyzer analyzer) {
+    // Method to parse the FT document collection - given the document collection file path, the analyzer and the index writer
+    public void FTParser(String filePath, Analyzer analyzer, IndexWriter iwriter) {
+        // Get a list of all files in the root file collection directory
         File dir = new File(filePath);
         File[] dirList = dir.listFiles();
 
+        // Declare an array to store the file list for every subsequent directory in the root directory
         ArrayList<String> fileList = new ArrayList<>();
 
+        // If there's files in the root directory, loop through each file, check it's a directory and add the file contents to a list
         if (dirList != null) {
-
             for (int i=0; i<dirList.length; i++) {
                 if (dirList[i].isDirectory()) {
                     for (File f: dirList[i].listFiles()) {
@@ -151,18 +168,18 @@ public class ParseIndexDocs {
             }
         }
 
+        // Declare some variables for parsing the document collection
         File file;
         org.jsoup.nodes.Document document;
         Elements elements;
-        
         String id;
         String title;
         String author;
         String body;
-
         ArrayList<Document> documentsList = new ArrayList<Document>();
         Document doc = new Document();
 
+        // For loop - loop through each file in the list, parse the contents into a document list and add it to the index
         try {
             for (String name: fileList) {
                 file = new File(name);
@@ -186,30 +203,34 @@ public class ParseIndexDocs {
 
                     doc = new Document();
                 }
-                writeToIndex(documentsList, analyzer);
+                // writeToIndex(documentsList, analyzer);
+                iwriter.addDocuments(documentsList);
                 documentsList = new ArrayList<>();
             }
         }
+        // Catch any exceptions
         catch (Exception e) {
-            System.out.println("Error: " + e);
+            System.out.println("Error in FTParser: " + e);
         }
     }
 
-    public void LATParser(String filePath, Analyzer analyzer) {
+    // Method to parse the LAT document collection - given the document collection file path, the analyzer and the index writer
+    public void LATParser(String filePath, Analyzer analyzer, IndexWriter iwriter) {
+        // Get a list of all folders in the root file collection directory
         File dir = new File(latPath);
         File[] fileList = dir.listFiles();
 
+        // Declare some variables for parsing the document collection
         ArrayList<Document> docList = new ArrayList<Document>();
         Document doc = new Document();
-
         String id;
         String title;
         String author;
         String body;
-
         org.jsoup.nodes.Document document;
         Elements elements;
 
+        // For loop - loop through each file in the directory, if it starts with "la", parse the contents into a document list and add it to the index
         try {
             for (File file: fileList) {
                 if (file.getName().startsWith("la")) {
@@ -233,50 +254,73 @@ public class ParseIndexDocs {
 
                         doc = new Document();
                     }
-                    writeToIndex(docList, analyzer);
+                    // writeToIndex(docList, analyzer);
+                    iwriter.addDocuments(docList);
                     docList = new ArrayList<>();
                 }   
             }
         }    
-        catch (IOException e) {
-            e.printStackTrace();
+        // Catch any exceptions
+        catch (Exception e) {
+            System.out.println("Error in LATParser: " + e);
         }
     }
 
-    private void writeToIndex(ArrayList<Document> docList, Analyzer analyzer) {
+    // private void writeToIndex(ArrayList<Document> docList, Analyzer analyzer) {
+    //     try {
+    //         Directory directory = FSDirectory.open(Paths.get(indexPath));
+    //         IndexWriterConfig config = new IndexWriterConfig(analyzer);
+    //         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+    //         IndexWriter iwriter = new IndexWriter(directory, config);
+
+    //         iwriter.addDocuments(docList);
+
+    //         iwriter.commit();
+    //         iwriter.close(); 
+    //         directory.close();
+    //     }
+    //     catch (Exception e) {
+    //         System.out.println("Error: " + e);
+    //     }
+    // }
+
+    // Method to call each parse method above and create an index from each returned document list
+    public void CallParsers(String code, Analyzer analyzer) {
         try {
+            // Open the index directory with an index writer
             Directory directory = FSDirectory.open(Paths.get(indexPath));
             IndexWriterConfig config = new IndexWriterConfig(analyzer);
             config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
             IndexWriter iwriter = new IndexWriter(directory, config);
 
-            iwriter.addDocuments(docList);
+            // Check what the document code is, and the appropriate parser to match the code
+            if (code == "fbis") {
+                FBISParser(fbisPath, analyzer, iwriter);
+                System.out.println("Added index: FBIS");
+            }
+            else if (code == "fr") {
+                FRParser(frPath, analyzer, iwriter);
+                System.out.println("Added index: FR");
+            }
+            else if (code == "ft") {
+                FTParser(ftPath, analyzer, iwriter);
+                System.out.println("Added index: FT");
+            }
+            else if (code == "lat") {
+                LATParser(latPath, analyzer, iwriter);
+                System.out.println("Added index: LAT");
+            }
 
+            // iwriter.addDocuments(docList);
+
+            // Close up all open objects
             iwriter.commit();
             iwriter.close(); 
             directory.close();
         }
+        // Catch any exceptions
         catch (Exception e) {
-            System.out.println("Error: " + e);
-        }
-    }
-
-    public void CallParsers(String code, Analyzer analyzer) {
-        if (code == "fbis") {
-            FBISParser(fbisPath, analyzer);
-            System.out.println("Added index: FBIS");
-        }
-        else if (code == "fr") {
-            FRParser(frPath, analyzer);
-            System.out.println("Added index: FR");
-        }
-        else if (code == "ft") {
-            FTParser(ftPath, analyzer);
-            System.out.println("Added index: FT");
-        }
-        else if (code == "lat") {
-            LATParser(latPath, analyzer);
-            System.out.println("Added index: LAT");
+            System.out.println("Error in CallParsers: " + e);
         }
     }
 }
